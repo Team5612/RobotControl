0dc9ab072e2342b7fc9631502be3fb2b67dbd6ff
Aurtik <txurtian@yahoo.com>
2015-02-15 14:42:19 -0500

    Working Servo

--- src/org/usfirst/frc/team5612/robot/Robot.java
+++ src/org/usfirst/frc/team5612/robot/Robot.java
@@ -1,20 +1,23 @@
 package org.usfirst.frc.team5612.robot;
 
+import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Relay;
-import edu.wpi.first.wpilibj.Relay.Value;
+//import edu.wpi.first.wpilibj.Relay;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 
 public class Robot extends IterativeRobot
-{
+{  
 	RobotDrive robotDrive; //Defines the class for basic drive operations
 	Joystick driveStick; //Defines the class for joy stick control
 	
-	Relay liftDrive; //Defines the class for the serveo
-	Joystick liftStick; //Defines the class for lift joy stick control
+	Talon liftServo;//Defines the class for the serveo
+	Joystick liftStick;//Defines the class for lift joy stick control
+	
+	CameraServer server;
 	
 	int autoLoopCounter;
 	
@@ -26,6 +29,13 @@ public class Robot extends IterativeRobot
 	{
 		robotDrive = new RobotDrive(0,1);
 		driveStick = new Joystick(0);
+		
+		liftServo = new Talon(3);
+		liftStick = new Joystick(3);
+		
+		server = CameraServer.getInstance();
+        server.setQuality(50);
+        server.startAutomaticCapture("cam0");
 	}
 	
 	/**
@@ -57,28 +67,17 @@ public class Robot extends IterativeRobot
 	 */
 	public void teleopInit()
 	{
-		liftDrive = new Relay(3);
-		liftStick = new Joystick(3);
-		liftDrive.notifyAll();
 		
-		if(liftDrive.get() == Value.kOff)
-		{
-			liftDrive.set(Value.kOn);
-		}
-		else
-		{
-			liftDrive.set(Value.kOff);
-		}
 	}
 	
 	/**
 	 * This function is called during user control
 	 */
 	public void teleopPeriodic()
-	{
+	{	
 		robotDrive.arcadeDrive(-driveStick.getY(), driveStick.getX());
+		liftServo.set(liftStick.getY());
 		Timer.delay(0.01);
-				 
 	}
 	
 	
